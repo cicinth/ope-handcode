9101d9e37da474f843bba2e03401dfb48e0c255e
alexcologia33 <alexcologia33@gmail.com>
2018-06-01 21:12:11 -0300

    filtro da lista de grupos

--- core/views/PainelAdministradorGrupoView.py
+++ core/views/PainelAdministradorGrupoView.py
@@ -5,6 +5,14 @@ def listarGrupo (request):
 
     contexto = {}
 
-    contexto['grupos'] = Grupo.objects.all()
+    if ('ativo' in request.GET.keys() and request.GET['ativo'] == 'true'):
+        contexto['grupos'] = Grupo.objects.filter(ativo=True)
+    elif ('ativo' in request.GET.keys() and request.GET['ativo'] == 'false'):
+        contexto['grupos'] = Grupo.objects.filter(ativo=False)
+    else:
+        contexto['grupos'] = Grupo.objects.all()
+    
+
+    
 
     return render(request,"administrador/grupos/index.html", contexto)
