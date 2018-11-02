305bc3fcd57a66b248f68f3a87c304add47d638f
HenriqueCode <hborges9294@gmail.com>
2018-11-01 21:15:39 -0300

    Update EntregasRestController.kt

--- src/main/kotlin/br/com/ope/controller/rest/EntregasRestController.kt
+++ src/main/kotlin/br/com/ope/controller/rest/EntregasRestController.kt
@@ -2,9 +2,12 @@ package br.com.ope.controller.rest
 
 import br.com.ope.model.Entrega
 import br.com.ope.repository.EntregaRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping(value = ["/rest/entregas", "/api/v1/entregas"])
@@ -24,5 +27,26 @@ class EntregasRestController {
             return entregaRepository.findAllByStatus(status)
         }
     }
+    
+    @GetMapping("/{id}/entregar")
+    fun entregar(@PathVariable id: UUID) : ResponseEntity<Any>  {
+
+        val optionalEntrega = entregaRepository.findById(id)
+
+        if (!optionalEntrega.isPresent) {
+            return ResponseEntity.notFound().build()
+        }
+
+        val entrega = optionalEntrega.get()
+
+        entrega.dataEntrega = Date()
+
+        entrega.status = Entrega.Status.REALIZADA
+
+        entregaRepository.save(entrega)
+
+        return ResponseEntity.ok(entrega)
+    }
+    
 
 }
