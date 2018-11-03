8bad1be5fdb99a43e803f3cac342536dc14b09d8
alexcologia33 <alexcologia33@gmail.com>
2018-11-03 16:23:51 -0200

    envio de arquivo tarefa

--- src/main/kotlin/br/com/ope/model/Arquivo.kt
+++ src/main/kotlin/br/com/ope/model/Arquivo.kt
@@ -1,6 +1,5 @@
 package br.com.ope.model
 
-import java.util.*
 import javax.persistence.Entity
 
 @Entity
@@ -8,7 +7,6 @@ class Arquivo : AbstractModel{
     var nome : String = ""
     var extensao : String = ""
     var mimeType : String = ""
-    var referencia : UUID = UUID.randomUUID()
 
     constructor() : super()
 }
